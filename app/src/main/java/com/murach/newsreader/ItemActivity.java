3aeab115b67224cddb1517072cdbcb9f79734d4b
Matthew Magee <matthewmagee05@gmail.com>
2016-09-06 09:39:22 -0700

    Initial commit

---
+++ app/src/main/java/com/murach/newsreader/ItemActivity.java
@@ -0,0 +1,20 @@
+package com.murach.newsreader;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentActivity;
+
+public class ItemActivity extends FragmentActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/murach/newsreader/ItemsActivity.java
@@ -0,0 +1,23 @@
+package com.murach.newsreader;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentActivity;
+
+
+public class ItemsActivity extends FragmentActivity
+ {
+
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items);
+        
+
+    }
+    
+
+}
